a4e845b3857a05bd61aec97161ab19e35f78927a
the-beatle <marcae7@gmail.com>
2023-01-29 21:40:13 -0500

    client drop

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { logger } from "./lib";
-import dictionaryRoutes from "./dictionary-routes";
 import productRoutes from "./product-routes";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(logger);
-app.use(express.static("./client"));
-app.use("/dictionary", dictionaryRoutes);
 app.use("/products", productRoutes);
 
 export default app;
